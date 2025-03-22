0788ee9348b4af0ab8acdfe22a16b87b3c06e596
Chantelle Osafo <chantelleosafo@gmail.com>
2025-03-22 17:53:05 +0000

    deploying

--- main.py
+++ main.py
@@ -194,3 +194,7 @@ async def delete_tag(tag_name: str):
     except Exception:
         raise HTTPException(status_code=404, detail="Database not found. Kindly set credentials above.") 
     return {"message": f"Tag '{tag_name}' deleted successfully"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
